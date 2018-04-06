9faebada1ce96fd71c30759be8ea3a09499efaa4
onurcankaya <onur.eren.cankaya@gmail.com>
2018-04-06 08:27:39 +0200

    added test for ExpenseDashboardPage component

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import toJSON from 'enzyme-to-json'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
 import Header from '../../components/header'
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />)
-  expect(toJSON(wrapper)).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot()
 
   // expect(wrapper.find('h1').length).toBe(1)
   // const renderer = new ReactShallowRenderer()
